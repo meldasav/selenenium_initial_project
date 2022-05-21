f69f6348b1faac86d52b5604251c70c7eb2e6152
meldasav <gmeldag83@hotmail.com>
2022-05-21 09:58:53 -0500

    still working

--- src/Driver.java
+++ src/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
     public static WebDriver getDriver(){
         if(driver == null){
 
-        //    System.setProperty("");
+            System.setProperty("WebDriver.chrome.driver","/Users/meldasav/IdeaProjects/selenium_initial_project/chromedriver");
             // Create the object of the web browser that you are automating
             driver = new ChromeDriver();
             driver.manage().window().maximize();
